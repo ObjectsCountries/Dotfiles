4ec98a1c6c62b23a123752fc92091950df7cb0f5
ObjectsCountries <kasramoayedi@protonmail.com>
2025-03-25 15:19:50 -0700

    Automate Hyprsunset

--- Wallpaper/set_wallpaper.py
+++ Wallpaper/set_wallpaper.py
@@ -2,22 +2,20 @@
 
 from datetime import datetime, timedelta, timezone
 from os import getenv
-import re
-import signal
+from signal import SIGTERM
 import subprocess
-import time
+from time import sleep
 
 from dotenv import load_dotenv
 import psutil
 from pysolar.solar import get_azimuth
-import pywal
 
 load_dotenv()
 
 wallpaper: int = -1
 previous_wallpaper: int = -1
 
-time.sleep(.5)
+sleep(.5)
 
 while True:
     date: datetime = datetime.now(timezone(-timedelta(hours=7))) # Modify according to your timezone
@@ -26,10 +24,24 @@ while True:
     wallpaper_path = f"/home/kasra/Dotfiles/Wallpaper/dear-{wallpaper}.jpg"
 
     if previous_wallpaper != wallpaper:
+
+        hyprsunset = None
+
+        for p in psutil.process_iter(["name"]):
+            if p.info["name"] == "hyprsunset":
+                hyprsunset = p
+                break
+
+        if wallpaper in {1, 2, 3, 11, 12} and hyprsunset is None:
+            subprocess.Popen(["hyprsunset", "-i", "5000"])
+        elif wallpaper in {4, 5, 6, 7, 8, 9, 10} and hyprsunset is not None:
+            hyprsunset.send_signal(SIGTERM)
+
         subprocess.run(["hyprctl", "hyprpaper", "preload", wallpaper_path])
         subprocess.run(["hyprctl", "hyprpaper", "wallpaper", ","+wallpaper_path])
         subprocess.run(["hyprctl", "hyprpaper", "unload", "unused"])
-        time.sleep(1)
+        sleep(1)
         subprocess.run(["hyprpanel", "sw", wallpaper_path])
+
     previous_wallpaper = wallpaper
-    time.sleep(60)
+    sleep(60)
