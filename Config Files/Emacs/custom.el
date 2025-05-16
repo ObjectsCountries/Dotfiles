00e558b8d42884f15e64c10f574a66ee080ad6de
ObjectsCountries <kasramoayedi@protonmail.com>
2025-05-15 21:02:41 -0700

    Add Emacs dots

---
+++ Config Files/Emacs/custom.el
@@ -0,0 +1,20 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elcord-display-line-numbers nil)
+ '(elcord-editor-icon "emacs_pen_icon")
+ '(elcord-mode t nil (elcord))
+ '(package-selected-packages
+   '(auto-complete good-scroll lsp-treemacs smooth-scroll tree-sitter-langs
+     verilog-ext)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'customize-group 'disabled nil)
+(put 'customize-variable 'disabled nil)
