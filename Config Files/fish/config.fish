a0be25f9faf28694714c8ab8010d614f32cde3c2
ObjectsCountries <kasramoayedi@protonmail.com>
2025-08-30 16:36:55 -0700

    Add fzf to fish

--- Config Files/fish/config.fish
+++ Config Files/fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     starship init fish | source
     alias eza='eza -l --color=always --icons=always --hyperlink -a --group-directories-first --git --total-size --no-user --no-time'
     set -gx EDITOR nvim
+    fzf --fish | source
 end
