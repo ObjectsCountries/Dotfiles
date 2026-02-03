a6648f9b0097904920c6f8ac3e2bddad313c5f1c
ObjectsCountries <kasramoayedi@protonmail.com>
2026-02-02 20:09:27 -0800

    some stupid fish stuff idk

--- Config Files/fish/config.fish
+++ Config Files/fish/config.fish
@@ -2,5 +2,6 @@ if status is-interactive
     starship init fish | source
     fzf --fish | source
     tree-sitter complete --shell=fish | source
+    # openclaw completion --shell fish | source
     alias eza='eza -l --color=always --icons=always --hyperlink -a --group-directories-first --git --total-size --no-user --no-time'
 end
